568174e1a5b5f57b8e8ba830fdd970945b502f5e
Raghulprakash1728 <raghulprakash08.r@gmail.com>
2022-12-15 23:35:09 +0530

    selenium

---
+++ Java/src/day3/OrangeHRM_Login.java
@@ -0,0 +1,32 @@
+package day3;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class OrangeHRM_Login {
+
+	public static void main(String[] args)
+	{
+	System.setProperty("webdriver.chrome.driver","F:\\selenium\\chromedriver_win32\\chromedriver.exe");
+	WebDriver driver = new ChromeDriver();
+	driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+	driver.manage().window().maximize();
+	
+	driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+	
+	WebElement UserName = driver.findElement(By.name("username"));
+	UserName.sendKeys("Admin");
+	
+	WebElement Password = driver.findElement(By.name("password"));
+	Password.sendKeys("admin123");
+	
+	WebElement Login = driver.findElement(By.xpath("//*[@class='oxd-button oxd-button--medium oxd-button--main orangehrm-login-button']"));
+	Login.click();
+	}
+
+}
